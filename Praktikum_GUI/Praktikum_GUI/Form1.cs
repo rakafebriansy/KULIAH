0d0d334594c3742705b2ead09bbe11b926079186
rakafebriansy <kerenr445@gmail.com>
2023-10-10 07:58:04 +0700

    finish login pt

--- Praktikum_GUI/Praktikum_GUI/Form1.cs
+++ Praktikum_GUI/Praktikum_GUI/Form1.cs
@@ -1,8 +1,11 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
 namespace Praktikum_GUI
 {
     public partial class Form1 : Form
     {
-        private string username = "Raka";
+        private string username1 = "Bima";
+        private string username2 = "Ando";
         private string password = "12345";
         public Form1()
         {
@@ -11,14 +14,19 @@ namespace Praktikum_GUI
 
         private void label3_Click(object sender, EventArgs e)
         {
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (textBox1.Text == username && textBox2.Text == password)
+            if ((textBox1.Text == username1 || textBox1.Text == username2) && textBox2.Text == password)
             {
-                
+                Form2 form_utama = new Form2(textBox1.Text);
+                form_utama.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Username atau Password salah!");
             }
         }
     }
--- Praktikum_GUI/Praktikum_GUI/Form2.cs
+++ Praktikum_GUI/Praktikum_GUI/Form2.cs
@@ -10,11 +10,18 @@ using System.Windows.Forms;
 
 namespace Praktikum_GUI
 {
+
     public partial class Form2 : Form
     {
-        public Form2()
+        public Form2(string username)
         {
             InitializeComponent();
+            label2.Text = $"sepuh {username}🙏";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
